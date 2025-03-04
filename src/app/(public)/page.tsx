f4e83c9ee5902a23195a8025d8f248ca2e8fa9d1
all <allmacdev@gmail.com>
2025-03-04 18:24:35 -0300

    home page

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header.Root>
+        <Header.Title />
+      </Header.Root>
+    </div>
+  );
+}
