bf62bd9ebe2ce05691fe8f2c8d6e85907c79110d
all <allmacdev@gmail.com>
2025-03-06 14:58:53 -0300

    fix redirect after login

--- src/app/(public)/(auth)/login/page.tsx
+++ src/app/(public)/(auth)/login/page.tsx
@@ -20,7 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function PaginaLogin() {
   const router = useRouter();
